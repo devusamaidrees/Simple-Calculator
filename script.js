4956f1d2395b49f6e6d8381d01dc07820104aa75
devusamaidrees <113791111+devusamaidrees@users.noreply.github.com>
2023-04-11 01:08:23 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,27 @@ let buttons=document.querySelectorAll(".btn")
 Array.from(buttons).forEach((btn)=>{
     btn.addEventListener("click",(e)=>{
         if (e.target.innerHTML=="=") {
+            let lastindex=string.length-1
+            if(string[lastindex] == "²")
+            {
+                string = string.slice(0,-1);
+                document.querySelector('.input_text').value = string;
+                string=string*string
+            }
+            else if(string[lastindex] == "³")
+                {
+                    string = string.slice(0,-1);
+                    document.querySelector('.input_text').value = string;
+                    string=string*string*string
+                }
+                for(let arr=0;arr<string.length-1;arr++){
+                    if(string[arr]=="x") {
+                        string=string.replaceAll("x","*");
+                    }
+                    else if (string[arr]=="÷") {
+                        string=string.replaceAll("÷","/")
+                    }
+                }
             string=eval(string)
             document.querySelector('.input_text').value=string
             string=""
@@ -16,25 +37,22 @@ Array.from(buttons).forEach((btn)=>{
             i="1"
         }
         else if (e.target.innerHTML == "Del") {
-            string = string.slice(0, -1);
+            string = string.slice(0,-1);
             document.querySelector('.input_text').value = string;
-        }
+            i="1"
+        }        
         else if (e.target.innerHTML=="x²") {
             if(i=="1"){
-            temp=string
-            temp=temp+"²"
-            document.querySelector('.input_text').value=temp
-            string=string*string
+            string=string+"²"
+            document.querySelector('.input_text').value=string
             i="0"
         }
         }
         else if (e.target.innerHTML=="x³") {
             if(i=="1")
             {
-            temp=string
-            temp=temp+"³"
-            document.querySelector('.input_text').value=temp
-            string=string*string*string
+            string=string+"³"
+            document.querySelector('.input_text').value=string
             i="0"
             }
     }
@@ -42,16 +60,21 @@ Array.from(buttons).forEach((btn)=>{
             if (i=="0") {
                 string=temp
             }
-        string=string+"*"
+            else{
+        string=string+"x"
         document.querySelector('.input_text').value=string
+            }
         }
         else if (e.target.innerHTML=="÷") {
             if (i=="0") {
                 string=temp
             }
-            string=string+"/"
+            else{
+            string=string+"÷"
             document.querySelector('.input_text').value=string
             }
+            }
+
         else{
             if(i=="1")
             {
